3d111549c6848eb01d515a4d63c1dff26e7d374b
Bob Park <hwpark11@kakao.com>
2022-11-20 21:26:27 +0900

    Initialize project

---
+++ oauth2client/src/main/java/com/bob/oauth2client/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.bob.oauth2client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping(path = "")
+    public String index() {
+        return "index";
+    }
+}
