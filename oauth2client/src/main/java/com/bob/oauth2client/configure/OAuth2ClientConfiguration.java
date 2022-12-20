992ad6b2330b97e6b5a77bb08561cd571e970c43
Bob Park <hwpark11@kakao.com>
2022-12-20 21:38:50 +0900

    feat: OAuth 2.0 oidc logout 설정 추가

--- oauth2client/src/main/java/com/bob/oauth2client/configure/OAuth2ClientConfiguration.java
+++ oauth2client/src/main/java/com/bob/oauth2client/configure/OAuth2ClientConfiguration.java
@@ -1,29 +1,51 @@
 package com.bob.oauth2client.configure;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfiguration {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests ->
             requests
-                .antMatchers("/loginPage").permitAll()
-                .antMatchers("/user").permitAll()
-                .antMatchers("/oidc").permitAll()
+//                .antMatchers("/loginPage").permitAll()
+//                .antMatchers("/user").permitAll()
+//                .antMatchers("/oidc").permitAll()
                 .anyRequest().authenticated());
 
 //        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
         http.oauth2Login(Customizer.withDefaults());
 
+        http.logout()
+            .logoutSuccessHandler(oidcLogoutSuccessHandler())
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .deleteCookies("JSESSIONID");
+
         return http.build();
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+
+        return successHandler;
     }
 
 
