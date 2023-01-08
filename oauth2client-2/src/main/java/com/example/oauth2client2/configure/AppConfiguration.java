008eb6cdb7da2e64cb092bc8b4be37dc87216647
Bob Park <hwpark11@kakao.com>
2023-01-08 21:38:14 +0900

    feat: Resource Owner Password 권한 부여 구현 (1) 예제 소스 추가

--- oauth2client-2/src/main/java/com/example/oauth2client2/configure/AppConfiguration.java
+++ oauth2client-2/src/main/java/com/example/oauth2client2/configure/AppConfiguration.java
@@ -1,13 +1,24 @@
 package com.example.oauth2client2.configure;
 
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
 
 @Configuration
 public class AppConfiguration {
@@ -29,8 +40,29 @@ public class AppConfiguration {
             new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
 
         auth2AuthorizedClientManager.setAuthorizedClientProvider(auth2AuthorizedClientProvider);
+        auth2AuthorizedClientManager.setContextAttributesMapper(contextAttributeMapper());
 
         return auth2AuthorizedClientManager;
+    }
+
+    private Function<OAuth2AuthorizeRequest, Map<String, Object>> contextAttributeMapper() {
+        return oauth2request -> {
+
+            Map<String, Object> contextAttribute = Maps.newHashMap();
+
+            HttpServletRequest request =
+                oauth2request.getAttribute(HttpServletRequest.class.getName()); // HttpServletRequest 얻어오기
+
+            String username = request.getParameter(OAuth2ParameterNames.USERNAME);
+            String password = request.getParameter(OAuth2ParameterNames.PASSWORD);
+
+            if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+                contextAttribute.put(OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, username);
+                contextAttribute.put(OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, password);
+            }
+
+            return contextAttribute;
+        };
     }
 
 }
--- oauth2client-2/src/main/java/com/example/oauth2client2/controller/LoginController.java
+++ oauth2client-2/src/main/java/com/example/oauth2client2/controller/LoginController.java
@@ -3,18 +3,49 @@ package com.example.oauth2client2.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @Controller
 public class LoginController {
 
+
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
+    private final OAuth2AuthorizedClientRepository authorizedClientRepository;
+
     @GetMapping(path = "oauth2Login")
     public String oauth2Login(Model model, HttpServletRequest request, HttpServletResponse response) {
+
+        // 아직 인증받지 못한 authentication 이므로, anonymous 가 됨
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        OAuth2AuthorizeRequest authorizeRequest =
+            OAuth2AuthorizeRequest.withClientRegistrationId("keycloak")
+                .principal(authentication)
+                .attribute(HttpServletRequest.class.getName(), request)
+                .attribute(HttpServletResponse.class.getName(), response)
+                .build();
+
+        // null 이 아닌 경우가 바로 인가를 받은 상태
+        // client 는 인가를 받은 상태지만, user 는 아직 인증받지 않은 상태이므로 anonymousUser 가 됨
+        OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+
+        if (authorizedClient != null) {
+
+        }
+
         return "redirect:/";
     }
 
