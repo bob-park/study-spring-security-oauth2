623694e185365cbc137ef99e078138f0f57e383b
Bob Park <hwpark11@kakao.com>
2023-01-29 19:44:49 +0900

    feat: OAuth 2.0 Resource Server JwtDecoder 설정 및 분석 예제 소스 추가

--- oauth2-resource-server/src/main/java/com/example/oauth2resourceserver/configure/OAuth2ResourceServerConfiguration.java
+++ oauth2-resource-server/src/main/java/com/example/oauth2resourceserver/configure/OAuth2ResourceServerConfiguration.java
@@ -1,9 +1,14 @@
 package com.example.oauth2resourceserver.configure;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,43 @@ public class OAuth2ResourceServerConfiguration {
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
         return http.build();
+    }
+
+    /**
+     * ! JwtDecoder 는 1개만 Bean 을 생성할 수 있다.
+     *
+     * @param properties
+     * @return
+     */
+//    @Bean
+//    public JwtDecoder jwtDecoder1(OAuth2ResourceServerProperties properties) {
+//        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+//    }
+
+    /**
+     * * OIDC Issuer URI
+     *
+     * @param properties
+     * @return
+     */
+//    @Bean
+//    public JwtDecoder jwtDecoder2(OAuth2ResourceServerProperties properties) {
+//        return JwtDecoders.fromOidcIssuerLocation(properties.getJwt().getIssuerUri());
+//    }
+
+    /**
+     * * NimubsJwtDecoder 를 바로 생성
+     * <p>
+     * - 별도의 설정을 추가로 할 수 있다.
+     *
+     * @param properties
+     * @return
+     */
+    @Bean
+    public JwtDecoder jwtDecoder2(OAuth2ResourceServerProperties properties) {
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
+            .jwsAlgorithm(SignatureAlgorithm.RS512)
+            .build();
     }
 
 }
