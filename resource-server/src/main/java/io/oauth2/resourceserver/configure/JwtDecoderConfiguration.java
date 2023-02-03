1b3b2ad3c858262c20c7633bad92ea0dc39f7eec
Bob Park <hwpark11@kakao.com>
2023-02-03 20:44:57 +0900

    feat: MAC 검증 기능 구현 - JwtDecoder 에 의한 검증 예제 소스 추가

---
+++ resource-server/src/main/java/io/oauth2/resourceserver/configure/JwtDecoderConfiguration.java
@@ -0,0 +1,25 @@
+package io.oauth2.resourceserver.configure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+
+@Configuration
+public class JwtDecoderConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.security.oauth2.resourceserver.jwt", name = "jws-algorithms", havingValue = "HS256")
+    public JwtDecoder jwtDecoderBySecretKeyValue(OctetSequenceKey octetSequenceKey,
+        OAuth2ResourceServerProperties properties) {
+        return NimbusJwtDecoder.withSecretKey(octetSequenceKey.toSecretKey())
+            .macAlgorithm(MacAlgorithm.from(properties.getJwt().getJwsAlgorithms().get(0)))
+            .build();
+    }
+
+}
--- resource-server/src/main/java/io/oauth2/resourceserver/configure/OAuth2ResourceServerConfiguration.java
+++ resource-server/src/main/java/io/oauth2/resourceserver/configure/OAuth2ResourceServerConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -40,13 +41,16 @@ public class OAuth2ResourceServerConfiguration {
                 .anyRequest().authenticated());
 
         http.userDetailsService(userDetailsService());
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//            .addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
         return http.build();
     }
 
-    @Bean
+//    @Bean
     public JwtAuthorizationMacFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationMacFilter(octetSequenceKey);
     }
